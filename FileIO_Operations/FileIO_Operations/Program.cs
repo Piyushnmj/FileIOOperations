542378b87bd4b8c98b245804f284412c388f9047
Piyushnmj <piyushnmj@yahoo.com>
2022-12-11 10:57:32 +0530

    Welcome

---
+++ FileIO_Operations/FileIO_Operations/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FileIO_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to File Input-Output Operations Program");
+        }
+    }
+}
